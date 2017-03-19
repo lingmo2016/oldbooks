c6248188c6300cc44f12c475c9f0d7cca149ddd4
邢悦 <2326319498@qq.com>
2017-03-19 10:31:02 +0800

    second

---
+++ app/src/main/java/com/xy/oldbooks/ui/activity/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package com.xy.oldbooks.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xingyue on 2017/3/18.
+ */
+
+public class WelcomeActivity extends AppCompatActivity{
+
+}
